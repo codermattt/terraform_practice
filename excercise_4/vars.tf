ad064f14cfe93bcaac01b4e4bf1ac2f5f727883e
codermattt <123294359+codermattt@users.noreply.github.com>
2025-06-05 15:56:49 +0200

    Add files via upload

---
+++ excercise_4/vars.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "zone1" {
+  default = "us-east-1a"
+}
+
+variable "webuser" {
+  default = "ubuntu"
+}
+
+variable "amiID" {
+  type = map(string)
+  default = {
+    "us-east-1" = "ami-084568db4383264d4" # Example AMI ID for us-east-1
+    "us-east-2" = "ami-04f167a56786e4b09"
+  }
+
+}
